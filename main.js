58ea3d16f475afa30d806e7ff523282e91d8a549
HasitaMandala <74747774+HasitaMandala@users.noreply.github.com>
2022-03-17 19:37:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+status = "";
+html = "";
+
+function setup(){
+    canvas = createCanvas(480,380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+   
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: detecting objects";
+    user_name = document.getElementById("user_name").value;
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380);
+
+}
+
+function modelLoaded(){
+    console.log("modelLoaded");
+    status = true;
+}
+
+function gotResult(error, results){
+
+}
